33adcd6bf5e7ae56fe3f2de6b6183d84fd76f3fd
PowerKuy <arieldany09@gmail.com>
2021-05-01 03:59:06 +0700

    Initial commit

---
+++ app/src/main/java/com/powerkuy/botyt/itemsupdater/MainActivity.java
@@ -0,0 +1,15 @@
+package com.powerkuy.botyt.itemsupdater;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+}
